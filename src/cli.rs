b04e390ba58148d4d9dcc73ec67ee3830d328506
Jeremy S <Jeremy@jsmoog.com>
2024-05-07 16:36:50 +0100

    Initial commit

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,36 @@
+use clap::{Args, Parser, Subcommand};
 
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// Increase verbosity of the command
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Commands that solely encrypt data
+    Encrypt {},
+    /// Commands that solely decrypt data
+    Decrypt {},
+    /// Commands that additionally use git
+    Git {},
+}
+
+#[derive(Args)]
+#[group(multiple = false)]
+struct PasswordOptions {
+    /// Read the password from the given environment variable
+    #[arg(long)]
+    password_env: String,
+    /// Read the password from the given file
+    #[arg(long)]
+    password_file: String,
+    /// Provide the password as an argument
+    #[arg(long)]
+    password: String,
+}
